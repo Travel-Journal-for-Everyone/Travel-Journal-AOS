c286c0311d6da7859f20d8db5dbc36591f324a1a
TedMoon99 <mtj1015@naver.com>
2025-03-03 17:20:52 +0900

    TJFE-52 [chore] Dependency 정리

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -41,23 +41,25 @@ android {
 
 dependencies {
 
+    // testImplementation
+    testImplementation(libs.junit)
+
+    // androidTestImplementation
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // implementation
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.ui.graphics.android)
     implementation(libs.androidx.material3.android)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    // Jetpack Compose 관련 의존성 추가
     implementation(libs.ui)
     implementation(libs.androidx.material)
     implementation(libs.ui.tooling.preview)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    // Dependency Setting
     implementation(project(":domain"))
-    // Dagger-Hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
