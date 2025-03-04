37afccbd1b51a1110c1a9c58433bc7c02528d2cb
TedMoon99 <mtj1015@naver.com>
2025-03-04 23:37:08 +0900

    TJFE-9 [fix] TokenService.kt url 경로 추가

--- data/src/main/java/com/tedmoon99/data/token/datasource/TokenService.kt
+++ data/src/main/java/com/tedmoon99/data/token/datasource/TokenService.kt
@@ -4,9 +4,11 @@ import com.tedmoon99.data.token.model.RefreshToken
 import com.tedmoon99.data.token.model.RefreshTokenDto
 import com.tedmoon99.data.token.model.TokenApiResponse
 import retrofit2.Response
+import retrofit2.http.POST
 
 interface TokenService {
 
+    @POST("v1/tokens/reissue")
     suspend fun refreshToken(request: RefreshTokenDto): Response<TokenApiResponse<RefreshToken>>
 
 }
