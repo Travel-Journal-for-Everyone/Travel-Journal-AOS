3a21df868e661852ce9b021649e9b6580b0a933d
TedMoon99 <mtj1015@naver.com>
2025-03-04 20:17:47 +0900

    TJFE-9 [test] AndroidTest를 위한 CustomTestRunner( -> AndroidJUnitRunner) 추가

---
+++ app/src/androidTest/java/com/tedmoon99/trave_journal_for_everyone/common/CustomTestRunner.kt
@@ -0,0 +1,16 @@
+package com.tedmoon99.trave_journal_for_everyone.common
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class CustomTestRunner: AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
