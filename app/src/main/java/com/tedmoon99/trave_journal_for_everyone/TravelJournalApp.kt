a1c2b327bf4b40004d3157f22c66c5157ef2a894
TedMoon99 <mtj1015@naver.com>
2025-03-07 14:52:31 +0900

    TJFE-58 [chore] kakao 초기화

--- app/src/main/java/com/tedmoon99/trave_journal_for_everyone/TravelJournalApp.kt
+++ app/src/main/java/com/tedmoon99/trave_journal_for_everyone/TravelJournalApp.kt
@@ -1,8 +1,14 @@
 package com.tedmoon99.trave_journal_for_everyone
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class TravelJournalApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
+    }
 }
