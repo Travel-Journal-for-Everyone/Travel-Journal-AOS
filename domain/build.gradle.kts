3c5397d34dd294f48b49bede6c7aa755e32125e6
TedMoon99 <mtj1015@naver.com>
2025-03-08 19:26:40 +0900

    TJFE-58 [chore] Dagger Gralde 설정

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("com.google.devtools.ksp")
 }
 
 java {
@@ -12,4 +13,14 @@ kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+
+    // Dagger Core (순수 Kotlin 모듈에서 Dagger 사용)
+    implementation(libs.dagger)
+    ksp(libs.dagger.compiler)
+
+    // Kotlin Coroutines (비동기 처리)
 }
