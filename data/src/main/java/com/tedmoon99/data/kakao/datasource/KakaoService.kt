4ab0fff600e25eff46f8498d37b660d6399337a8
TedMoon99 <mtj1015@naver.com>
2025-03-08 19:27:53 +0900

    TJFE-58 [Feat] Kakao Repository 구현

--- data/src/main/java/com/tedmoon99/data/kakao/datasource/KakaoService.kt
+++ data/src/main/java/com/tedmoon99/data/kakao/datasource/KakaoService.kt
@@ -3,15 +3,13 @@ package com.tedmoon99.data.kakao.datasource
 import com.tedmoon99.data.kakao.model.KakaoSignIn
 import retrofit2.Response
 import retrofit2.http.POST
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface KakaoService {
     // Kakao SignIn
-    @POST("v1/auth/login/{socialProvider}/id-token")
+    @POST("v1/auth/kakao/id-token-login/{deviceId}")
     suspend fun requestKakaoSignIn(
-        @Path("socialProvider") socialProvider: String,
-        @Query("deviceId") deviceId: String?
+        @Query("deviceId") deviceId: String,
     ): Response<KakaoSignIn>
 
 }
