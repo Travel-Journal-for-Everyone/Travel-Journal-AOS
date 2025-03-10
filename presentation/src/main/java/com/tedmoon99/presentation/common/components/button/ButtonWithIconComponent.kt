6e46417fc395469f2856706ed30bac856fa223f5
TedMoon99 <mtj1015@naver.com>
2025-03-10 09:59:38 +0900

    TJFE-58 [fix] 컴포넌트 인자 타입 수젖ㅇ

--- presentation/src/main/java/com/tedmoon99/presentation/common/components/button/ButtonWithIconComponent.kt
+++ presentation/src/main/java/com/tedmoon99/presentation/common/components/button/ButtonWithIconComponent.kt
@@ -1,10 +1,11 @@
 package com.tedmoon99.presentation.common.components.button
 
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,12 +19,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ButtonWithIconComponent(
     @DrawableRes icon: Int,
-    label: String,
+    @StringRes label: Int,
     containerColor: Color,
     contentColor: Color,
     iconColor: Color,
@@ -46,7 +48,7 @@ fun ButtonWithIconComponent(
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth().padding(vertical = 12.dp, horizontal = 16.dp),
+            modifier = modifier.padding(vertical = 12.dp, horizontal = 16.dp),
         ) {
             Icon(
                 painter = painterResource(icon),
@@ -58,7 +60,7 @@ fun ButtonWithIconComponent(
             Spacer(modifier = Modifier.width(8.dp))
 
             Text(
-                text = label,
+                text = stringResource(label),
             )
         }
     }
