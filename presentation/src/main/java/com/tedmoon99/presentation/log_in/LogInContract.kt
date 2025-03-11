91d463fb15abd7de48f82cfc144ae891c8b1d4b7
TedMoon99 <mtj1015@naver.com>
2025-03-11 17:04:34 +0900

    TJFE-58 [feat] 로그인 Contract 정의

---
+++ presentation/src/main/java/com/tedmoon99/presentation/log_in/LogInContract.kt
@@ -0,0 +1,24 @@
+package com.tedmoon99.presentation.log_in
+
+import com.tedmoon99.presentation.common.viewmodel.UiEffect
+import com.tedmoon99.presentation.common.viewmodel.UiEvent
+import com.tedmoon99.presentation.common.viewmodel.UiState
+
+object LogInContract {
+
+    sealed class Event : UiEvent {
+        data object KakaoLogInClicked : Event()
+        data object AppleLogInClicked : Event()
+        data object GoogleLogInClicked : Event()
+    }
+
+    data class State(
+        val isLoggedIn: Boolean = false,
+        val isLoading: Boolean = false
+    ) : UiState
+
+    sealed class Effect : UiEffect {
+        data object NavigateToHome: Effect()
+        data object ShowErrorMessage : Effect()
+    }
+}
