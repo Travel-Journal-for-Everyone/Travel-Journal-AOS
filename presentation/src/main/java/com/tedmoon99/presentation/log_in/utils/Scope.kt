a6bd208a37385f2858c78bcaeb68e72ef1f3ae6b
TedMoon99 <mtj1015@naver.com>
2025-03-13 19:41:44 +0900

    TJFE-8 [design] 공개범위 포함 데이터 추가

--- presentation/src/main/java/com/tedmoon99/presentation/log_in/utils/Scope.kt
+++ presentation/src/main/java/com/tedmoon99/presentation/log_in/utils/Scope.kt
@@ -1,7 +1,9 @@
 package com.tedmoon99.presentation.log_in.utils
 
-enum class Scope {
-    PUBLIC_SCOPE,
-    ONLY_FOLLOW_SCOPE,
-    PRIVATE_SCOPE,
+import com.tedmoon99.presentation.R
+
+enum class Scope(val title: String, val message: String, val icon: Int) {
+    PUBLIC_SCOPE("전체 공개", "모든 사용자가 볼 수 있어요", R.drawable.icon_world),
+    ONLY_FOLLOW_SCOPE("팔로우 공개", "나를 팔로우한 사용자만 볼 수 있어요.", R.drawable.icon_person),
+    PRIVATE_SCOPE("나만 보기", "나만 볼 수 있어요.", R.drawable.icon_lock),
 }
