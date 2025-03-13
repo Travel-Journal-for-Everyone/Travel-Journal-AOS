f069baae318c6963e64aaa1d4918173499e0b9d7
TedMoon99 <mtj1015@naver.com>
2025-03-13 23:23:52 +0900

    TJFE-8 [feat] MVI 패턴 적용 - WriteProfileContract.kt - WriteProfileViewModel.kt

---
+++ presentation/src/main/java/com/tedmoon99/presentation/log_in/WriteProfileContract.kt
@@ -0,0 +1,33 @@
+package com.tedmoon99.presentation.log_in
+
+import com.tedmoon99.presentation.common.viewmodel.UiEffect
+import com.tedmoon99.presentation.common.viewmodel.UiEvent
+import com.tedmoon99.presentation.common.viewmodel.UiState
+import com.tedmoon99.presentation.log_in.utils.Scope
+
+object WriteProfileContract {
+
+    sealed class Event : UiEvent {
+        data object ProfileImageClicked: Event()
+        data object DoubleCheckClicked: Event()
+        data object NameEntered: Event()
+        data class ScopeClicked(val scope: Scope): Event()
+    }
+
+    data class State(
+        val scope: Scope,
+        val name: String,
+        val isValidateName: Boolean = false,
+        val isNotDuplicatedName: Boolean = false,
+        val isLoading: Boolean = false,
+        val isScopeOpened: Boolean = false,
+    ): UiState
+
+    sealed class Effect : UiEffect {
+        data object NavigateToHome: Effect()
+        data object ShowErrorMessage: Effect()
+        data object ShowSuccessMessage: Effect()
+
+    }
+
+}
