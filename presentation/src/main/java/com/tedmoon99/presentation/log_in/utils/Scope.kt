46c9399974dbd1ac52be0a3664706140cf97b8d2
TedMoon99 <mtj1015@naver.com>
2025-03-13 19:02:04 +0900

    TJFE-8 [design] 공개 범위 정의

--- presentation/src/main/java/com/tedmoon99/presentation/log_in/utils/Scope.kt
+++ presentation/src/main/java/com/tedmoon99/presentation/log_in/utils/Scope.kt
@@ -1,9 +1,7 @@
 package com.tedmoon99.presentation.log_in.utils
 
-import com.tedmoon99.presentation.R
-
-enum class Scope(val title: String, val message: String, val icon: Int) {
-    PUBLIC_SCOPE("전체 공개", "모든 사용자가 볼 수 있어요", R.drawable.icon_world),
-    ONLY_FOLLOW_SCOPE("팔로우 공개", "나를 팔로우한 사용자만 볼 수 있어요.", R.drawable.icon_person),
-    PRIVATE_SCOPE("나만 보기", "나만 볼 수 있어요.", R.drawable.icon_lock),
+enum class Scope {
+    PUBLIC_SCOPE,
+    ONLY_FOLLOW_SCOPE,
+    PRIVATE_SCOPE,
 }
