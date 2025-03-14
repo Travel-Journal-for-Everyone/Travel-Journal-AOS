8d7c1a63ef837004f8e8a98bd09c0b7964155e97
TedMoon99 <mtj1015@naver.com>
2025-03-14 15:40:50 +0900

    TJFE-8 [feat] 회원가입 완료 domain 정의 - SignUpDtoEntity.kt - SignUpRepository.kt - SignUpResultEntity.kt - SignUpUseCase.kt - SignUpUseCaseImpl.kt

--- domain/src/main/java/com/tedmoon99/domain/member/repository/SignUpRepository.kt
+++ domain/src/main/java/com/tedmoon99/domain/member/repository/SignUpRepository.kt
@@ -1,8 +1,12 @@
 package com.tedmoon99.domain.member.repository
 
 import com.tedmoon99.domain.member.entity.DuplicatedCheckResultEntity
+import com.tedmoon99.domain.member.entity.SignUpDtoEntity
+import com.tedmoon99.domain.member.entity.SignUpResultEntity
 
 interface SignUpRepository {
 
     suspend fun checkDuplicatedName(name: String): DuplicatedCheckResultEntity
+
+    suspend fun completeSignUp(request: SignUpDtoEntity): SignUpResultEntity
 }
