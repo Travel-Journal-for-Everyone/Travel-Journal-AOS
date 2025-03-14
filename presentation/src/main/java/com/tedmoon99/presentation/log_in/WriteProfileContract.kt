ca87517b8b3ad6cc68d9d8b48404b4816f22dff5
TedMoon99 <mtj1015@naver.com>
2025-03-14 15:45:58 +0900

    TJFE-8 [feat] 회원가입 Effect 추가

--- presentation/src/main/java/com/tedmoon99/presentation/log_in/WriteProfileContract.kt
+++ presentation/src/main/java/com/tedmoon99/presentation/log_in/WriteProfileContract.kt
@@ -25,9 +25,10 @@ object WriteProfileContract {
 
     sealed class Effect : UiEffect {
         data object NavigateToHome: Effect()
-        data object ShowErrorMessage: Effect()
         data object ShowSuccessMessage: Effect()
-
+        data object ShowCompleteFailedMessage: Effect()
+        data object ShowDuplicatedErrorMessage: Effect()
+        data object ShowBadWordErrorMessage: Effect()
     }
 
 }
