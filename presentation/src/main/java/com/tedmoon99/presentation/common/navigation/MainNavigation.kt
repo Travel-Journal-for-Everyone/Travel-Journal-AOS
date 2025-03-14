d5c69dc65ba2fd75180a0be8cc98db679bc68397
TedMoon99 <mtj1015@naver.com>
2025-03-14 17:35:31 +0900

    TJFE-8 [design] WelcomeScreen.kt 추가

--- presentation/src/main/java/com/tedmoon99/presentation/common/navigation/MainNavigation.kt
+++ presentation/src/main/java/com/tedmoon99/presentation/common/navigation/MainNavigation.kt
@@ -1,13 +1,19 @@
 package com.tedmoon99.presentation.common.navigation
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.tedmoon99.presentation.log_in.view.LogInScreen
 import com.tedmoon99.presentation.home.view.HomeScreen
 import com.tedmoon99.presentation.common.screen.Screen
+import com.tedmoon99.presentation.log_in.view.WelcomeScreen
+import com.tedmoon99.presentation.log_in.view.WriteProfileScreen
 
 @Composable
 fun MainNavigation() {
@@ -19,19 +25,70 @@ fun MainNavigation() {
         startDestination = Screen.LogIn.route,
     ) {
         // Home
-        composable(Screen.Home.route){
+        composable(
+            route = Screen.Home.route,
+            enterTransition = { slideInHorizontally() },
+            exitTransition = { slideOutHorizontally() }
+        ){
             HomeScreen()
         }
 
         // LogIn
-        composable(Screen.LogIn.route){
+        composable(
+            route = Screen.LogIn.route,
+            exitTransition = { slideOutHorizontally() }
+        ){
             LogInScreen(
                 hostState = snackbarHostState,
-                onLogInSuccess = {
-
+                navigateToHome = {
+                    navController.navigate(Screen.Home.route){
+                        popUpTo(navController.graph.id) { inclusive = true }
+                    }
+                },
+                navigateToWriteProfile = {
+                    navController.navigate(Screen.WriteProfile.route)
                 }
             )
         }
 
+        // WriteProfile
+        composable(
+            route = Screen.WriteProfile.route,
+            enterTransition = { slideInHorizontally() },
+            exitTransition = { slideOutHorizontally() }
+        ){
+            WriteProfileScreen(
+                hostState = snackbarHostState,
+                navigateToWelcome = { name ->
+                    // Welcome 페이지로 이동
+                    navController.navigate(
+                        Screen.Welcome.route.replace("{name}", name)
+                    ){
+                        popUpTo(navController.graph.id) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        // Welcome
+        composable(
+            route = Screen.Welcome.route,
+            arguments = listOf(
+                navArgument("name") { type = NavType.StringType }
+            ),
+            enterTransition = { slideInHorizontally() },
+            exitTransition = { slideOutHorizontally() }
+        ){ navBackStackEntry ->
+            val name = navBackStackEntry.arguments?.getString("name") ?: ""
+
+            WelcomeScreen(
+                name = name,
+                navigateToHome = {
+                    navController.navigate(Screen.Home.route){
+                        popUpTo(navController.graph.id){ inclusive = true }
+                    }
+                }
+            )
+        }
     }
 }
