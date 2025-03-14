eef5dd78202553b495d38e3e1d85c2d31637d98c
TedMoon99 <mtj1015@naver.com>
2025-03-14 17:35:31 +0900

    TJFE-8 [design] WelcomeScreen.kt 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -64,4 +64,5 @@ dependencies {
     ksp(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
     implementation("com.kakao.sdk:v2-all:2.20.6") // 전체 모듈 설치 (로그인, 공유, 메시지, 피커, 내비, 인증)
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.33.2-alpha")
 }
