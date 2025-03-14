d13d37c222855dc0e8a539bc658a90ea679da6f0
TedMoon99 <mtj1015@naver.com>
2025-03-14 15:41:44 +0900

    TJFE-8 [feat] 회원가입 완료 data 정의 - DuplicatedCheckMapper.kt - SignUpDto.kt - SignUpMapper.kt - SignUpRepositoryImpl.kt - SignUpResult.kt - SignUpService.kt

--- data/src/main/java/com/tedmoon99/data/member/mapper/SignUpMapper.kt
+++ data/src/main/java/com/tedmoon99/data/member/mapper/SignUpMapper.kt
@@ -1,23 +1,37 @@
 package com.tedmoon99.data.member.mapper
 
-import com.tedmoon99.data.member.model.DuplicatedCheckResult
-import com.tedmoon99.domain.member.entity.DuplicatedCheckResultEntity
+import com.tedmoon99.data.member.model.SignUpDto
+import com.tedmoon99.data.member.model.SignUpResult
+import com.tedmoon99.domain.member.entity.SignUpDtoEntity
+import com.tedmoon99.domain.member.entity.SignUpResultEntity
 
 object SignUpMapper {
 
-    fun fromDomain(domain: DuplicatedCheckResultEntity): DuplicatedCheckResult {
-        return DuplicatedCheckResult(
-            success = domain.success,
-            isDuplicated = domain.isDuplicated,
-            isBadWorld = domain.isBadWorld
+    fun fromDomain(domain: SignUpDtoEntity): SignUpDto {
+        return SignUpDto(
+            nickname = domain.nickname,
+            accountScope = domain.accountScope
         )
     }
 
-    fun toDomain(data: DuplicatedCheckResult): DuplicatedCheckResultEntity {
-        return DuplicatedCheckResultEntity(
+    fun toDomain(data: SignUpDto): SignUpDtoEntity {
+        return SignUpDtoEntity(
+            nickname = data.nickname,
+            accountScope = data.accountScope
+        )
+    }
+
+    fun resultFromDomain(domain: SignUpResultEntity): SignUpResult {
+        return SignUpResult(
+            success = domain.success,
+            message = domain.message
+        )
+    }
+
+    fun resultToDomain(data: SignUpResult): SignUpResultEntity {
+        return SignUpResultEntity(
             success = data.success,
-            isDuplicated = data.isDuplicated,
-            isBadWorld = data.isBadWorld
+            message = data.message
         )
     }
 }
