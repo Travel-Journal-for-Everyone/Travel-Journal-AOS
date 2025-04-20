a63b3d97fa05aba960de15d76adfb7d315a3d0cc
songjuseok <realsongzi93@gmail.com>
2025-04-20 22:16:21 +0900

    build.gradle.kts
    Coil3 설정

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -65,4 +65,6 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
     implementation("com.kakao.sdk:v2-all:2.20.6") // 전체 모듈 설치 (로그인, 공유, 메시지, 피커, 내비, 인증)
     implementation("com.google.accompanist:accompanist-navigation-animation:0.33.2-alpha")
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 }
