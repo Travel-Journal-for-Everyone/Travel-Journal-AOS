43912eebaf025a721dbd6378cd3edd6812c38840
TedMoon99 <mtj1015@naver.com>
2025-05-03 00:01:05 +0900

    TJFE-115 [feat] Search 관련 Paging 로직 처리 1. PagingSource 정의 - SearchUserPagingSource.kt
    2. Dto 정의
    - SearchUserDto.kt
    - SortX.kt
    - User.kt
    - Pageable.kt
    - CommonUtils.kt

---
+++ data/src/main/java/com/tedmoon99/data/search/model/user/SearchUserDto.kt
@@ -0,0 +1,29 @@
+package com.tedmoon99.data.search.model.user
+
+
+import com.google.gson.annotations.SerializedName
+
+data class SearchUserDto(
+    @SerializedName("content")
+    val userList: List<User>,
+    @SerializedName("empty")
+    val empty: Boolean,
+    @SerializedName("first")
+    val first: Boolean,
+    @SerializedName("last")
+    val last: Boolean,
+    @SerializedName("number")
+    val number: Int,
+    @SerializedName("numberOfElements")
+    val numberOfElements: Int,
+    @SerializedName("pageable")
+    val pageable: Pageable,
+    @SerializedName("size")
+    val size: Int,
+    @SerializedName("sort")
+    val sort: SortX,
+    @SerializedName("totalElements")
+    val totalElements: Int,
+    @SerializedName("totalPages")
+    val totalPages: Int
+)
