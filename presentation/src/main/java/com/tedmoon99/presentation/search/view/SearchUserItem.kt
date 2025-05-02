cd879f9965132a5243f742be4630b0d2a9c71096
TedMoon99 <mtj1015@naver.com>
2025-05-03 00:04:00 +0900

    TJFE-115 [remove] 불필요한 import문 제거

--- presentation/src/main/java/com/tedmoon99/presentation/search/view/SearchUserItem.kt
+++ presentation/src/main/java/com/tedmoon99/presentation/search/view/SearchUserItem.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
