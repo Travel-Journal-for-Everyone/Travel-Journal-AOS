267cd828b29f9da2566348a69c2cdd7535c5ad65
TedMoon99 <mtj1015@naver.com>
2025-05-19 20:38:17 +0900

    TJFE-165 [design] UI Component 생성 - ButtonComponent.kt - BasicTopAppbarComponent.kt - OutlinedButtonComponent.kt

--- presentation/src/main/java/com/tedmoon99/presentation/common/components/appbar/BasicTopAppbarComponent.kt
+++ presentation/src/main/java/com/tedmoon99/presentation/common/components/appbar/BasicTopAppbarComponent.kt
@@ -4,14 +4,17 @@ import androidx.annotation.DrawableRes
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import com.tedmoon99.presentation.R
+import com.tedmoon99.presentation.common.theme.White
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,6 +34,7 @@ fun BasicTopAppbarComponent(
                 text = title,
                 textAlign = TextAlign.Center,
                 modifier = modifier,
+                style = MaterialTheme.typography.titleSmall
             )
         },
         navigationIcon = {
@@ -64,7 +68,8 @@ fun BasicTopAppbarComponent(
                     onClick = {}
                 ) { }
             }
-        }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = White),
     )
 
 }
